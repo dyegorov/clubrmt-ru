a0a8be69e19c7c9a71c44f0690c82322e453a626
Dmitry Egorov <d.yegorov@gmail.com>
2023-12-17 17:42:31 +0300

    add sea movies

--- sea-movies/index.php
+++ sea-movies/index.php
@@ -1,131 +1,83 @@
-<?
+<?php
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Фильмы о море");
 ?>
     <section class="article">
         <div class="container">
             <div class="bread">
-                <a href="/">Главная</a>
-                <a href="#" class="active">Фильмы о море</a>
+                <a href="/">Главная</a> <a href="#" class="active">Фильмы о море</a>
             </div>
-
-            <h1 class="h1">Фильмы о море</h1>
-
-            <div class="videos__grid">
-                <a href="#" class="news__item">
-                    <div class="news__img">
-                        <img src="<?= SITE_TEMPLATE_PATH ?>/img/vv1.66617309.jpg" alt=""/>
-                    </div>
-                    <div class="news__content">
-                        <h3 class="news__title">
-                            Фрагменты из документального фильма Крейсер Варяг , 2004 года
-                            (кадры спуска вельбота). Фильм снят к 100-летию подвига крейсера
-                        </h3>
-                        <div class="news__look">
-                            посмотреть <img src="<?= SITE_TEMPLATE_PATH ?>/img/goto.67391245.svg" alt=""/>
-                        </div>
-                    </div>
-                </a>
-                <a href="#" class="news__item">
-                    <div class="news__img">
-                        <img src="<?= SITE_TEMPLATE_PATH ?>/img/vv2.b4d45f5f.jpg" alt=""/>
-                    </div>
-                    <div class="news__content">
-                        <h3 class="news__title">
-                            Военно-морская шлюпка и ее роль на флоте
-                        </h3>
-                        <div class="news__look">
-                            посмотреть <img src="<?= SITE_TEMPLATE_PATH ?>/img/goto.67391245.svg" alt=""/>
-                        </div>
-                    </div>
-                </a>
-                <a href="#" class="news__item">
-                    <div class="news__img">
-                        <img src="<?= SITE_TEMPLATE_PATH ?>/img/v3.493d4d8b.jpg" alt=""/>
-                    </div>
-                    <div class="news__content">
-                        <h3 class="news__title">Россия и Англия на суше и море</h3>
-                        <div class="news__look">
-                            посмотреть <img src="<?= SITE_TEMPLATE_PATH ?>/img/goto.67391245.svg" alt=""/>
-                        </div>
-                    </div>
-                </a>
-                <a href="#" class="news__item">
-                    <div class="news__img">
-                        <img src="<?= SITE_TEMPLATE_PATH ?>/img/v4.ee0d540a.jpg" alt=""/>
-                    </div>
-                    <div class="news__content">
-                        <h3 class="news__title">Зарождение русского флота</h3>
-                        <div class="news__look">
-                            посмотреть <img src="<?= SITE_TEMPLATE_PATH ?>/img/goto.67391245.svg" alt=""/>
-                        </div>
-                    </div>
-                </a>
-                <a href="#" class="news__item">
-                    <div class="news__img">
-                        <img src="<?= SITE_TEMPLATE_PATH ?>/img/v5.28ee70d1.jpg" alt=""/>
-                    </div>
-                    <div class="news__content">
-                        <h3 class="news__title">
-                            Морской быт, корабельный устав и русский дух
-                        </h3>
-                        <div class="news__look">
-                            посмотреть <img src="<?= SITE_TEMPLATE_PATH ?>/img/goto.67391245.svg" alt=""/>
-                        </div>
-                    </div>
-                </a>
-                <a href="#" class="news__item">
-                    <div class="news__img">
-                        <img src="<?= SITE_TEMPLATE_PATH ?>/img/v6.f0ed2f25.jpg" alt=""/>
-                    </div>
-                    <div class="news__content">
-                        <h3 class="news__title">
-                            Развитие парусно-гребных судов ВМФ России
-                        </h3>
-                        <div class="news__look">
-                            посмотреть <img src="<?= SITE_TEMPLATE_PATH ?>/img/goto.67391245.svg" alt=""/>
-                        </div>
-                    </div>
-                </a>
-                <a href="#" class="news__item">
-                    <div class="news__img">
-                        <img src="<?= SITE_TEMPLATE_PATH ?>/img/v7.61c6b1f5.jpg" alt=""/>
-                    </div>
-                    <div class="news__content">
-                        <h3 class="news__title">Про шлюпки, парус и морские традиции</h3>
-                        <div class="news__look">
-                            посмотреть <img src="<?= SITE_TEMPLATE_PATH ?>/img/goto.67391245.svg" alt=""/>
-                        </div>
-                    </div>
-                </a>
-                <a href="#" class="news__item">
-                    <div class="news__img">
-                        <img src="<?= SITE_TEMPLATE_PATH ?>/img/v8.e2891096.jpg" alt=""/>
-                    </div>
-                    <div class="news__content">
-                        <h3 class="news__title">
-                            Трафальгар Балтики: Забытые победы русско-шведской войны
-                        </h3>
-                        <div class="news__look">
-                            посмотреть <img src="<?= SITE_TEMPLATE_PATH ?>/img/goto.67391245.svg" alt=""/>
-                        </div>
-                    </div>
-                </a>
-                <a href="#" class="news__item">
-                    <div class="news__img">
-                        <img src="<?= SITE_TEMPLATE_PATH ?>/img/v9.def5e703.jpg" alt=""/>
-                    </div>
-                    <div class="news__content">
-                        <h3 class="news__title">
-                            Развитие парусно-гребных судов ВМФ России
-                        </h3>
-                        <div class="news__look">
-                            посмотреть <img src="<?= SITE_TEMPLATE_PATH ?>/img/goto.67391245.svg" alt=""/>
-                        </div>
-                    </div>
-                </a>
-            </div>
-
+            <?php $APPLICATION->IncludeComponent(
+                "bitrix:news",
+                "sea-movies",
+                array(
+                    "ADD_ELEMENT_CHAIN" => "N",
+                    "ADD_SECTIONS_CHAIN" => "Y",
+                    "AJAX_MODE" => "N",
+                    "AJAX_OPTION_ADDITIONAL" => "",
+                    "AJAX_OPTION_HISTORY" => "N",
+                    "AJAX_OPTION_JUMP" => "N",
+                    "AJAX_OPTION_STYLE" => "Y",
+                    "BROWSER_TITLE" => "-",
+                    "CACHE_FILTER" => "N",
+                    "CACHE_GROUPS" => "Y",
+                    "CACHE_TIME" => "36000000",
+                    "CACHE_TYPE" => "A",
+                    "CHECK_DATES" => "Y",
+                    "DETAIL_ACTIVE_DATE_FORMAT" => "d.m.Y",
+                    "DETAIL_DISPLAY_BOTTOM_PAGER" => "Y",
+                    "DETAIL_DISPLAY_TOP_PAGER" => "N",
+                    "DETAIL_FIELD_CODE" => array("", ""),
+                    "DETAIL_PAGER_SHOW_ALL" => "Y",
+                    "DETAIL_PAGER_TEMPLATE" => "",
+                    "DETAIL_PAGER_TITLE" => "Страница",
+                    "DETAIL_PROPERTY_CODE" => array("", "[YOUTUBE_PREVIEW]", ""),
+                    "DETAIL_SET_CANONICAL_URL" => "N",
+                    "DISPLAY_BOTTOM_PAGER" => "Y",
+                    "DISPLAY_DATE" => "Y",
+                    "DISPLAY_NAME" => "Y",
+                    "DISPLAY_PICTURE" => "Y",
+                    "DISPLAY_PREVIEW_TEXT" => "Y",
+                    "DISPLAY_TOP_PAGER" => "N",
+                    "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                    "IBLOCK_ID" => "9",
+                    "IBLOCK_TYPE" => "info",
+                    "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+                    "LIST_ACTIVE_DATE_FORMAT" => "d.m.Y",
+                    "LIST_FIELD_CODE" => array("", ""),
+                    "LIST_PROPERTY_CODE" => array("", ""),
+                    "MESSAGE_404" => "",
+                    "META_DESCRIPTION" => "-",
+                    "META_KEYWORDS" => "-",
+                    "NEWS_COUNT" => "50",
+                    "PAGER_BASE_LINK_ENABLE" => "N",
+                    "PAGER_DESC_NUMBERING" => "N",
+                    "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                    "PAGER_SHOW_ALL" => "N",
+                    "PAGER_SHOW_ALWAYS" => "N",
+                    "PAGER_TEMPLATE" => "pager",
+                    "PAGER_TITLE" => "Новости",
+                    "PREVIEW_TRUNCATE_LEN" => "",
+                    "SEF_MODE" => "N",
+                    "SET_LAST_MODIFIED" => "N",
+                    "SET_STATUS_404" => "Y",
+                    "SET_TITLE" => "Y",
+                    "SHOW_404" => "N",
+                    "SORT_BY1" => "ACTIVE_FROM",
+                    "SORT_BY2" => "SORT",
+                    "SORT_ORDER1" => "DESC",
+                    "SORT_ORDER2" => "ASC",
+                    "STRICT_SECTION_CHECK" => "N",
+                    "USE_CATEGORIES" => "N",
+                    "USE_FILTER" => "N",
+                    "USE_PERMISSIONS" => "N",
+                    "USE_RATING" => "N",
+                    "USE_REVIEW" => "N",
+                    "USE_RSS" => "N",
+                    "USE_SEARCH" => "N",
+                    "USE_SHARE" => "N",
+                    "VARIABLE_ALIASES" => array("ELEMENT_ID" => "ELEMENT_ID", "SECTION_ID" => "SECTION_ID")
+                )
+            ); ?>
         </div>
-    </section>
-    </section><? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
+    </section> <br><?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
